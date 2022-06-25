7fb53d257250aba489eabbf5a8b8f3e03e49f018
hamsof <hafizabdulman@gmail.com>
2022-06-25 10:49:55 +0500

    pagination completed

--- Version 9 (pagination)/index.js
+++ Version 9 (pagination)/index.js
@@ -1,26 +1,3 @@
-
-var previous_elem;
-
-function load_books_index(index_1,index_2,elem) {
-if(index_1 !== 0)
-{
-    previous_elem.classList.remove('active')
-}
-previous_elem = elem
-elem.classList.add("active")
-
-fetch("books.json")
-        .then((res) => {
-            return res.json();
-        })
-        .then(data =>   {
-            for(let i=index_1; i<index_2;i++)
-            {
-                load_books(data[i])
-            }
-        })
-}
-
 function load_books(index){
     const book = document.createElement("div")
     book.classList.add("book_container","col-sm-4")
@@ -41,35 +18,82 @@ function load_books(index){
                 </div>
     `
     container = document.getElementById("container")
-    container.innerHTML = ""
     container.appendChild(book)
 }
 
-// .addEventListener("click", load_books_index(0,15));
+//getting id of the element that fired that event
+//behtreen logic 
+
+
+function load_books_by_page(e)
+{
+    console.log(e);
+    console.log(e.target.id);
+    Id_fired = e.target.id;
+    var index_1;
+    var index_2;
+    next_button = document.getElementById("page_next")
+    if(Id_fired == "page_1")
+    {
+        index_1 = 0;
+        index_2 = 15;
+    }
+    if(Id_fired == "page_2")
+    {
+        index_1 = 15;
+        index_2 = 30;
+    }
+    if(Id_fired == "page_3")
+    {
+        index_1 = 30;
+        index_2 = 45;
+    }
+    if(Id_fired == "page_4")
+    {
+        index_1 = 45;
+        index_2 = 60;
+    }
+    if(Id_fired == "page_5")
+    {
+        index_1 = 60;
+        index_2 = 75;
+    }
+    if(Id_fired == "page_6")
+    {
+        index_1 = 75;
+        index_2 = 90;
+    }
+    if(Id_fired == "page_7")
+    {
+        index_1 = 90;
+        index_2 = 103;
+    }
+    if(Id_fired == "page_next")
+    {
+        index_1 = 75;
+        index_2 = 90;
+    }     
+    container = document.getElementById("container")
+    container.innerHTML = ""   
+    fetch("books.json")
+            .then((res) => {
+                return res.json();
+            })
+            .then(data =>   {
+                for(let i=index_1;i<index_2;i++)
+                {
+                    load_books(data[i])
+                }
+})
+}
 
 for(let i=1;i<=7;i++)
 {
     var e = "page_" + i;
     var elem = document.getElementById(e)
     index_1 = 0
-    elem.addEventListener('click',/*load_books_index(index_1,15+index_1,elem)*/
-        (index_1,index_2,elem) =>{
-            if(index_1 !== 0)
-            {
-                previous_elem.classList.remove('active')
-            }
-            previous_elem = elem
-            elem.classList.add("active")
-            
-            fetch("books.json")
-                    .then((res) => {
-                        return res.json();
-                    })
-                    .then(data =>   {
-                        for(let i=index_1; i<index_2;i++)
-                        {
-                            load_books(data[i])
-                        }
-        })
-})
+    elem.addEventListener('click',load_books_by_page)
+    
 }
+page_1 = document.getElementById("page_1")
+page_1.click()
